b127429fb09ce256eee0b98dd2200e0967d40e54
MK793 <66381718+MK793@users.noreply.github.com>
2023-01-04 18:32:51 +0000

    Create exercise1.py

---
+++ exercise1.py
@@ -0,0 +1,9 @@
+# Write a program to add two numbers together
+# The code must be concise with comments describing what each line of code does
+
+def main():
+  sum1 = 10
+  sum2 = 20
+  total = 0
+  
+main()
